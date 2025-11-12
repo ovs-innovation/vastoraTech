e41757ad22adc2ecc851d3c67956dce80fa81e6f
Angadkumar07 <angadkumar70676@gmail.com>
2025-11-12 14:27:55 +0530

    fixed type error3

--- frontend/src/app/blog-masonry/page.tsx
+++ frontend/src/app/blog-masonry/page.tsx
@@ -1,8 +1,8 @@
-"use client";
-
-import ScrollToTop from '@/components/common/scroll-to-top';
+import dynamic from "next/dynamic";
 import BlogMasonry from '@/components/inner-pages/blog-masonry';
 import Wrapper from '@/layout/Wrapper';
+
+const ScrollToTop = dynamic(() => import('@/components/common/scroll-to-top'), { ssr: false });
 
 export const metadata = {
     title: "Blog Masonry - SEO Marketing - Digital Marketing & SEO Agency Next js Template",
