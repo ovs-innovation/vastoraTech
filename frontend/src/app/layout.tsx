c8886ed10e99dfaa3423d784b2565f5e7625d986
Angadkumar07 <angadkumar70676@gmail.com>
2026-02-07 13:54:36 +0530

    added facbook pixel code

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -44,6 +44,26 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             `,
           }}
         />
+        {/* Meta Pixel Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '767820493070214');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
@@ -56,6 +76,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        {/* Meta Pixel Code (noscript) */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=767820493070214&ev=PageView&noscript=1"
+            alt="facebook-pixel"
+          />
+        </noscript>
+        {/* End Meta Pixel Code (noscript) */}
         {/* <TawkMessenger /> */}
         {children}
         <WhatsappButton />
